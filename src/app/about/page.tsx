f42baede79b4a2221dabdccc1a221c9ba7149119
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-02-04 23:40:02 +0800

    Add about page, github and devpost link

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { Title } from "@mantine/core";
+
+export default function AboutPage() {
+    return (
+        <div>
+            <Title>About Us</Title>
+        </div>
+    )
+}
--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import {
+	ActionIcon,
 	AppShell,
 	Burger,
+	Flex,
+	NavLink,
+	ScrollArea,
+	Tooltip,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconBook } from "@tabler/icons-react";
+import {
+	IconAddressBook,
+	IconBook,
+	IconBrandDisqus,
+	IconBrandGithub,
+	IconHome2,
+} from "@tabler/icons-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
 	children,
@@ -13,6 +26,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	const [opened, { toggle }] = useDisclosure();
+	const path = usePathname();
 
 	return (
 		<AppShell
@@ -36,10 +50,59 @@ export default function RootLayout({
 					Lecture to Toktik
 				</div>
 			</AppShell.Header>
-			<AppShell.Navbar p="md">Navbar</AppShell.Navbar>
-			<AppShell.Main>
-				{children}
-			</AppShell.Main>
+			<AppShell.Navbar p="md">
+				<AppShell.Section grow component={ScrollArea}>
+					<NavLink
+						component={Link}
+						href="/"
+						active={path === "/"}
+						label="Lecture Summariser"
+						leftSection={<IconHome2 size="1rem" stroke={1.5} />}
+					/>
+					<NavLink
+						component={Link}
+						href="/about"
+						active={path === "/about"}
+						label="About Us"
+						leftSection={
+							<IconAddressBook size="1rem" stroke={1.5} />
+						}
+					/>
+				</AppShell.Section>
+				<AppShell.Section className="border-t border-gray-400/20 pt-2">
+					<Flex justify="flex-end">
+						<Tooltip label="Github">
+							<ActionIcon
+								size={42}
+								variant="subtle"
+								c="dimmed"
+								aria-label="Github Link"
+								component="a"
+								href="https://github.com/tohhongxiang123/techfest-2024"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<IconBrandGithub />
+							</ActionIcon>
+						</Tooltip>
+						<Tooltip label="Devpost">
+							<ActionIcon
+								size={42}
+								variant="subtle"
+								c="dimmed"
+								aria-label="Github Link"
+								component="a"
+								href="https://devpost.com/software/placeholder-7bhy40?ref_content=user-portfolio&ref_feature=in_progress"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<IconBrandDisqus />
+							</ActionIcon>
+						</Tooltip>
+					</Flex>
+				</AppShell.Section>
+			</AppShell.Navbar>
+			<AppShell.Main>{children}</AppShell.Main>
 		</AppShell>
 	);
 }
