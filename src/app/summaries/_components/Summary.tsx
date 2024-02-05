340b3b16de992afe576504b77e469fecf5f99180
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-02-05 21:21:38 +0800

    Add fetching from backend

--- src/app/summaries/_components/Summary.tsx
+++ src/app/summaries/_components/Summary.tsx
@@ -4,17 +4,23 @@ import { Container } from "@mantine/core";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
 
-export default async function Summary({ id = "" }: { id: string }) {
-	const data = await getSummary(id);
-	const mdxSource = await serialize(data.summary, {
-		mdxOptions: {
-			development: process.env.NODE_ENV === "development",
-		},
-	});
+export default async function Summary({
+  id = "",
+  type = "youtube",
+}: {
+  id: string;
+  type: "youtube" | "file";
+}) {
+  const data = await getSummary(id, type);
+  const mdxSource = await serialize(data.summary, {
+    mdxOptions: {
+      development: process.env.NODE_ENV === "development",
+    },
+  });
 
-	return (
-		<Container className="prose">
-			<MDXRemote {...mdxSource} />
-		</Container>
-	);
+  return (
+    <Container className="prose">
+      <MDXRemote {...mdxSource} />
+    </Container>
+  );
 }
