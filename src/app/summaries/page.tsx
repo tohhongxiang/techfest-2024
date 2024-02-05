3eae2ddbbf248b286641d4c359fd3e87905713fc
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-02-06 00:18:47 +0800

    Update content for generated summary examples

--- src/app/summaries/page.tsx
+++ src/app/summaries/page.tsx
@@ -4,32 +4,33 @@ import { Card, Stack, Title } from "@mantine/core";
 import Link from "next/link";
 
 export default function SummaryPage() {
-	return (
-		<Stack>
-			<Title>Summaries by You</Title>
-			<ul className="flex flex-col gap-y-4">
-				<li>
-					<Link href="/summaries/123">
-						<Card shadow="sm" padding="lg" radius="md" withBorder>
-							Summary 123
-						</Card>
-					</Link>
-				</li>
-				<li>
-					<Link href="/summaries/768">
-						<Card shadow="sm" padding="lg" radius="md" withBorder>
-							Summary 768
-						</Card>
-					</Link>
-				</li>
-				<li>
-					<Link href="/summaries/5463">
-						<Card shadow="sm" padding="lg" radius="md" withBorder>
-							Summary 5463
-						</Card>
-					</Link>
-				</li>
-			</ul>
-		</Stack>
-	);
+    return (
+        <Stack>
+            <Title>Summaries by You</Title>
+            <ul className="flex flex-col gap-y-4">
+                <li>
+                    <Link href="/summaries/youtube/b-Bb_TyhC1A">
+                        <Card shadow="sm" padding="lg" radius="md" withBorder>
+                            The Search for the Longest Infinite Chess Game
+                        </Card>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/summaries/youtube/yg16u_bzjPE">
+                        <Card shadow="sm" padding="lg" radius="md" withBorder>
+                            Would you sacrifice one person to save five? -
+                            Eleanor Nelsen
+                        </Card>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/summaries/youtube/i53Gi_K3o7I">
+                        <Card shadow="sm" padding="lg" radius="md" withBorder>
+                            20 System Design Concepts Explained in 10 Minutes
+                        </Card>
+                    </Link>
+                </li>
+            </ul>
+        </Stack>
+    );
 }
